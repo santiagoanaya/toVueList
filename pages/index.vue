bc6e0a65ca7973e4c2a662c49e04cc5462678520
santiagoanaya <anayasantiagohernan@gmail.com>
2018-07-05 18:14:53 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  div.container
+    h1 Welcome!
+    p
+      nuxt-link(to="/list") To-Vue List - Never forget your tasks
+</template>
+
+<style>
+  .container {
+    font: "Quicksand";
+    padding: 60px;
+  }
+</style>
---
+++ pages/list.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  div.container
+    h1 To-Vue List
+    p
+      nuxt-link(exact to="/") Home page
+    h3 Add your tasks
+    input(v-model="task",placeholder="Add a task")
+    button(@click="addTask") Add
+
+    ul
+      li(v-for="t in tasks")
+        b {{ t }}
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      task:'',
+      tasks:[]
+    }
+  },
+  methods: {
+    addTask() {
+      this.tasks.push(this.task)
+      this.task = ''
+    },
+  }
+}
+</script>
+
+
+<style>
+  .container {
+    font: "Quicksand";
+    padding: 60px;
+  }
+</style>
