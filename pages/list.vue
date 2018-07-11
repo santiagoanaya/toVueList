608dac3b1d6562ababe33ed03c1d51b5098d2130
santiagoanaya <anayasantiagohernan@gmail.com>
2018-07-11 10:15:50 -0300

    Add functionality of marking a task as done

--- pages/list.vue
+++ pages/list.vue
@@ -4,21 +4,26 @@
     p
       nuxt-link(exact to="/") Home page
     h3 Add your tasks
-    input(v-model="task",placeholder="Add a task")
+    input(v-model="task", placeholder="Add a task")
     button(@click="addTask") Add
 
-    ul
+    ul Tasks
       li(v-for="t in tasks")
         b {{ t }}
+        button(@click="finishTask(t)") Finished
+
+    ul Completed
+      li(v-for="c in completed")
+        b {{ c }}
 </template>
 
 <script>
-
 export default {
   data () {
     return {
       task:'',
-      tasks:[]
+      tasks:[],
+      completed:[]
     }
   },
   methods: {
@@ -26,6 +31,10 @@ export default {
       this.tasks.push(this.task)
       this.task = ''
     },
+    finishTask(completedTask) {
+      this.completed.push(completedTask)
+      this.tasks.splice( this.tasks.indexOf(completedTask), 1 )
+    }
   }
 }
 </script>
